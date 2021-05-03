82ed942b10251424b68b98dbf6351bf0079bfed6
Kush2011 <74670716+Kush2011@users.noreply.github.com>
2021-05-03 21:03:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -27,9 +27,30 @@ function speak()
     synth.speak(utter_this);
 
     Webcam.attatch(camera);
+
+    setTimeout(function()
+    {
+        snapshot();
+        save();
+    }, 5000);
 }
 
 camera = document.getElementById("camera");
 Webcam.set({
     width: 360, height: 250, image_format:'jpg', jpg_quality: 19
 });
+
+function snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
